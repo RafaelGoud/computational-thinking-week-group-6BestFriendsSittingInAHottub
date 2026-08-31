1516c524efee6356762c62139466181d3e15fb88
donnagerholt <donna.gerholt@student.uva.nl>
2026-08-31 11:19:21 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,5 +1,5 @@
 from file_oliwia import my_namee
-from file_donna import function_name
+from file_donna import my_name
 from file_milan import get_name
 from name import get_namee
 from file_michelle import my_name
