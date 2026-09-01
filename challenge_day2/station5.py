9a58773ac5ee57855ce9cb5af5c37994059c4aa5
michelledoody27 <michelledoody27@gmail.com>
2026-09-01 13:47:27 +0200

    Create station5.py
    function for station 5

---
+++ challenge_day2/station5.py
@@ -0,0 +1,5 @@
+team_one = ["Ainas", "Tobit", "Yasmin", "Zoë", 
+            "Iuliia", "Oumaima", "Klementyna", "Markus", "Mufang",
+            "Ebony", "Yurui", "Nandini", "Nathan", "Tiara",
+            "Ben", "Yuvraj", "Christopher", "Lula", "Muni"]
+
