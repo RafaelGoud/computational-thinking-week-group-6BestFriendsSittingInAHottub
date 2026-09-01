00f7c9fb523a7ec1c841d4f25705666e0968fd93
Rafael Goudriaan <rafael.goudriaan@student.uva.nl>
2026-09-01 13:35:51 +0200

    Update station2.py

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -1,12 +1,12 @@
 def solution_station_2=(date_str):
     chinese_days = {
-        0: "星期一",  # Monday
-        1: "星期二",  # Tuesday
-        2: "星期三",  # Wednesday
-        3: "星期四",  # Thursday
-        4: "星期五",  # Friday
-        5: "星期六",  # Saturday
-        6: "星期日",  # Sunday
+        0: "星期一",
+        1: "星期二",
+        2: "星期三",
+        3: "星期四",
+        4: "星期五",
+        5: "星期六",
+        6: "星期日",
     }
     
     date_obj = datetime.strptime(date_str, "%Y-%m-%d")
